eb3a58c80793bc22a8f2a014c585735b47eca688
Jonathan Beluch <jon@jonathanbeluch.com>
2010-06-08 21:48:33 -0400

    added google calendar support

--- alarm.py
+++ alarm.py
@@ -3,10 +3,13 @@
 #6/2/2010 jon@jonathanbeluch.com
 from libs.BeautifulSoup import BeautifulStoneSoup as BSS
 import urllib2
+import datetime
 from string import Template
+from libs.rfc3339 import parse_datetime, parse_date, datetimetostr
 from ConfigParser import ConfigParser
 import re
 import os
+import urllib
 #collect data from each source, enter into the main festival script
 #1. download data
 #2. compose template for specific data
@@ -48,21 +51,44 @@ class GMail(object):
         return self.output
 
 class GoogleCalendar(object):
-    def __init__(self, url):
-        self.url = url
+    url = 'http://www.google.com/calendar/feeds/%s/private-%s/full-noattendees'
+
+    def __init__(self, options):
+        self.username = options['username']
+        self.cookie = options['cookie']
+        self.url = self.url % (self.username, self.cookie)
+        self.allday_tmpl = read_template(options['allday_template'])
+        self.timed_tmpl = read_template(options['timed_template'])
 
     def generate_output(self):
-        self._get_events()
-        return 'done' 
+        d = datetime.datetime.now()
 
-    def _get_events(self):
-        u = urllib2.urlopen(self.url)
-        events = BSS(u.read())
-        u.close()
-        event_tags = events.findAll('category')
-        for e in event_tags:
-            print e
-            print ''
+        #only request events that start within the next 24 hours
+        params = {'start-min': datetimetostr(d),
+                  'start-max': datetimetostr(d + datetime.timedelta(1)),
+                  'orderby': 'starttime',
+                  'sortorder': 'ascending'}
+        self.url = '%s?%s' % (self.url, urllib.urlencode(params))
+        xml = BSS(download_page(self.url), 
+                  selfClosingTags=['category', 'content', 'link'])
+
+        #only parse entry title and start time
+        entries = [(e.title.text, 
+                    get_datetime(e.find('gd:when')['starttime']))
+                    for e in xml.findAll('entry')]
+        output = []
+        for content, start_time in entries:
+            #use different templates for all day events vs events with a start
+            #time
+            if type(start_time) == datetime.date:
+                output.append(self.allday_tmpl.substitute(
+                    {'content': content}))
+            else:
+                output.append(self.timed_tmpl.substitute(
+                    {'content': content, 
+                     'start_time': start_time.strftime('%I:%M %p')}))
+        return ''.join(output) 
+
     
 class Weather(object):
 
@@ -96,6 +122,12 @@ class Weather(object):
             [wdata.update({'%s_%s' % (name, k): v}) for k, v in y.attrs]
         return(wdata)
 
+def get_datetime(date_string):
+    """takes an rfc3339 formatted date or datetime string and returns the apppropriate date or datetime object"""
+    if date_string.find('T') > 0:
+        return parse_datetime(date_string)
+    return parse_date(date_string)
+
 def create_output(config):
     output = {} 
 
@@ -107,7 +139,14 @@ def create_output(config):
     weather = Weather(dict(config.items('weather')))
     output['weather'] = weather.generate_output()
 
+    # Google Calendar
+    gcal = GoogleCalendar(dict(config.items('gcal')))
+    output['gcal'] = gcal.generate_output()
+
     print output
+    #for now just print to a file, eventually will use a master template here
+    with open('output', 'w') as f:
+        f.writelines(output.values())
 
 def get_config_options():
     config = ConfigParser()
